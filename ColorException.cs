2edba6ad1ccf0b6629e3329c1d09ba3f1683f134
Ian Baker <Ian.Baker@contractor.prometric.com>
2020-04-02 10:08:54 -0400

    Initial commit

---
+++ ColorException.cs
@@ -0,0 +1,30 @@
+﻿namespace ReColor
+{
+    using System;
+    using System.Text;
+
+    public class ColorException : Exception
+    {
+        public sealed override string Message { get; }
+
+        private string AvailableColors()
+        {
+            var sb = new StringBuilder();
+            foreach (var enumeration in Enum.GetValues(typeof(ReColor.Colors)))
+            {
+                sb.AppendLine(enumeration.ToString());
+            }
+            return sb.ToString();
+        }
+        
+        public ColorException()
+        {
+        }
+
+        public ColorException(string message) : base(message)
+        {
+             Message = $"Failed to parse '{message}' to a Color.";
+             if (ReColor.ExplicitExceptions) Message += $"Available choices are:\n{AvailableColors()}";
+        }
+    }
+}
---
+++ DiscordColorReader.cs
@@ -0,0 +1,29 @@
+﻿namespace ReColor
+{
+    using System;
+    using System.Globalization;
+    using System.Threading.Tasks;
+    using Discord;
+    using Discord.Commands;
+
+    public class DiscordColorReader : TypeReader
+    {
+        public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input,
+            IServiceProvider services)
+        {
+            //From name.
+            if (ReColor.ParseColorName(input, out Color namedColor))
+                return Task.FromResult(TypeReaderResult.FromSuccess(namedColor));
+
+            //From raw hexvalue.
+            if (int.TryParse(input, NumberStyles.HexNumber, null, out var intHex))
+                return Task.FromResult(TypeReaderResult.FromSuccess(intHex));
+
+            //From string hexvalue
+            if (ReColor.ParseHexValue(input, out Color stringHex))
+                return Task.FromResult(TypeReaderResult.FromSuccess(namedColor));
+
+            return Task.FromResult(TypeReaderResult.FromError(new ColorException(input)));
+        }
+    }
+}
